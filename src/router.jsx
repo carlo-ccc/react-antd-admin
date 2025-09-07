57ca27a7e7cb403538aebc822085631529193ea2
carlo-ccc <awiezzz@qq.com>
2025-09-08 00:47:23 +0800

    feat：zustand状态管理

--- src/router.jsx
+++ src/router.jsx
@@ -1,7 +1,18 @@
 import { createBrowserRouter } from 'react-router';
 import App from './App.jsx';
+import useStore from './store';
 
-const About = () => <>About</>;
+const About = () => {
+ const { count, increment, decrement } = useStore();
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+};
 
 export const router = createBrowserRouter([
   { path: '/', element: <App /> },
