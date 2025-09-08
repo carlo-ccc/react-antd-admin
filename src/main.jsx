d8f113eaf87d32de12ca2ae062a54e2793548631
carlo-ccc <awiezzz@qq.com>
2025-09-08 23:44:04 +0800

    feat：添加mantine

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import { router } from './router';
+import { MantineProvider } from '@mantine/core';
 
 createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+  <MantineProvider>
+    <RouterProvider router={router} />
+  </MantineProvider>
 );
